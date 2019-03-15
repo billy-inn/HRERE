c20bbe7dae673890c88aea7b18e177e644654aa3
bly <bly930725@gmail.com>
2019-03-14 22:18:30 -0400

    add prepare data

---
+++ data/prepare_data.py
@@ -0,0 +1,10 @@
+from gensim.scripts.glove2word2vec import glove2word2vec
+import os
+
+os.system("wget http://nlp.stanford.edu/data/glove.840B.300d.zip")
+os.system("unzip glove.840B.300d.zip")
+os.system("rm glove.840B.300d.zip")
+
+glove_file = "glove.840B.300d.txt"
+word2vec_file = "glove.840B.300d.w2v.txt"
+_ = glove2word2vec(glove_file, word2vec_file)
